05a34074df4aaf561bea2a204e4cec732d614074
euginirudayaraj <euginirudayaraj2002@gmail.com>
2026-06-03 17:44:39 +0530

    Add GitHub Actions Lambda deployment

--- lambdas/flowbridge-sbx-url-read.py
+++ lambdas/flowbridge-sbx-url-read.py
@@ -1,10 +1,6 @@
 import json
 import os
-test_eugin="Eugin-PC";
 
-eugin=os.getenv('TEST_EUGIN')
-rds_host = os.getenv('RDS_HOST')
-rds_port = os.getenv('RDS_PORT')
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
